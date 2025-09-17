0e68ab42bd29c44378cc5d8dbd8d47e9be232da7
shipra707 <amrendramishra156@gmail.com>
2025-09-17 10:22:18 +0530

    ADDED

---
+++ day6/function.js
@@ -0,0 +1,21 @@
+ function fun(){
+//     console.log("FSD Class");
+// }
+// console.log(fun);
+// console.log(fun())
+
+// const a = function b(){
+//     console.log("FSD Class!");
+// }
+// console.log(a());
+
+// arrow function
+console.log(a);
+const a = () => {
+    console.log("FSD");
+}
+console.log(a());
+
+() =>{
+    console.log("Helllo");
+}}
